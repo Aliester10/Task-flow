894fea37ac3398d23aa5ebf2ea3513de2c900814
aliester10 <kisjo.official0411@gmail.com>
2026-08-08 23:48:54 +0700

    fix(db): correct table name from User to users in migration script

--- backend/prisma/migrations/20260808162800_rbac_refresh_token/migration.sql
+++ backend/prisma/migrations/20260808162800_rbac_refresh_token/migration.sql
@@ -2,4 +2,4 @@
 ALTER TYPE "MemberRole" ADD VALUE 'ADMIN';
 
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN "token_version" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "users" ADD COLUMN "token_version" INTEGER NOT NULL DEFAULT 0;
